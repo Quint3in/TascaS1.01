16a258ba8e96411776e9c046c37e9dc657be6247
JoelQuintana <joelqn95@gmail.com>
2026-02-18 12:18:50 +0100

    feat(n3exercici1): add Writer class

---
+++ N3Exercici1/src/Writer.java
@@ -0,0 +1,12 @@
+public class Writer {
+    static final double SALARY_COMPANY = 1500;
+
+    private String name;
+    private final String DNI;
+    static double salary = SALARY_COMPANY;
+
+    public Writer(String DNI) {
+        this.DNI = DNI;
+    }
+
+}
